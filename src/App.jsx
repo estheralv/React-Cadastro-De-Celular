1b73f46e770cef2163e7a543213df1b2f4e7a535
Esther <125416224+estheralv@users.noreply.github.com>
2024-12-10 20:44:25 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CadastroDeCelular from './assets/components/CadastroDeCelular'
+
+function App() {
+  return (
+    <>
+    <CadastroDeCelular/>
+    </>
+  )
+}
+
+export default App
